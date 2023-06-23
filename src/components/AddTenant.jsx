1df168a37042fbeff8e0553ec771fc30aa0d50ff
Peter Chege <peterkagure@gmail.com>
2023-06-23 11:52:56 +0300

    Some change

---
+++ src/components/AddTenant.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AddTenant = () => {
+    return (
+        <div>
+            <input type="text"/>
+
+        </div>
+    );
+};
+
+export default AddTenant;
---
+++ src/util/BASE_URL.js
@@ -0,0 +1,11 @@
+
+
+
+const LOCAL_URL = "http://localhost:9000"
+const REMOTE_URL = ""
+
+
+const BASE_URL = LOCAL_URL
+
+
+export default BASE_URL
